46f6c537f81b920dfdbc3e2168d439696e511de3
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-10-26 19:56:03 -0700

    installed python environment and print is working correctly

---
+++ main.py
@@ -0,0 +1,12 @@
+#my age
+age = 25
+
+#when am i born
+print(2020 -age)
+
+#year when it was legal to drink
+print(2020 - age + 21)
+
+#minimum age he can date(how i met your mother)
+print(age/2 + 7)
+

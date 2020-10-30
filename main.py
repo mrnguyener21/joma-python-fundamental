093d76b971ab56be50926ca4668345ff4bcda08f
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-10-30 08:18:16 -0700

    failed recurssion problem 2 and 3

--- main.py
+++ main.py
@@ -1,239 +1,311 @@
-#my age
-# age = 25
+# #my age
+# # age = 25
 
-#when am i born
-# print(2020 -age)
+# #when am i born
+# # print(2020 -age)
 
-#year when it was legal to drink
-# print(2020 - age + 21)
+# #year when it was legal to drink
+# # print(2020 - age + 21)
 
-#minimum age he can date(how i met your mother)
-# print(age/2 + 7)
+# #minimum age he can date(how i met your mother)
+# # print(age/2 + 7)
 
-#data types and operators
-# print ( 4%3)
-# print (4>=4)
+# #data types and operators
+# # print ( 4%3)
+# # print (4>=4)
 
-#functions
-def hello(name):
-    greet = 'Hi ' + name
-    # print(greet)
-#anything indented is within the scope of the function
-hello('Jonathan');
+# #functions
+# def hello(name):
+#     greet = 'Hi ' + name
+#     # print(greet)
+# #anything indented is within the scope of the function
+# hello('Jonathan');
 
-#fucntions assignment 1 SCORE
-# Let's say that ever yday in joma class you get a quiz. you can eithe get the correct answer, the wrong answer or not do the quiz at all because you slept through your alarm and didn't show up to class. Each of them will have a different score:
-# correct answer: 2 pts
-# wrong answer: 1 pt 
-# no show: 0 pt 
-# write a function that produces your final score as a percentage given:
-# total: the number of quizzes
-# correct: the number of quizzes you answered
-# wrong: the number of quizzes you answered incorrectly 
-# (correct * 2 + wrong * 1) / (total * 2)
-def score(total, correct, wrong):
-    return ((correct * 2 + wrong * 1) / (total * 2)) * 100
-# print(score(20,0,20))
+# #fucntions assignment 1 SCORE
+# # Let's say that ever yday in joma class you get a quiz. you can eithe get the correct answer, the wrong answer or not do the quiz at all because you slept through your alarm and didn't show up to class. Each of them will have a different score:
+# # correct answer: 2 pts
+# # wrong answer: 1 pt 
+# # no show: 0 pt 
+# # write a function that produces your final score as a percentage given:
+# # total: the number of quizzes
+# # correct: the number of quizzes you answered
+# # wrong: the number of quizzes you answered incorrectly 
+# # (correct * 2 + wrong * 1) / (total * 2)
+# def score(total, correct, wrong):
+#     return ((correct * 2 + wrong * 1) / (total * 2)) * 100
+# # print(score(20,0,20))
 
-#FUNCTION ASSIGNMENT 2 SCORE OF BEST 75% DAYS
-# The same question as previously, however, we only take your best 75% days to account for sick days. No one is ever healthy every day. That means that even if they didn't get every question correct, as long as they got 75% of the questions correct, they will get 100%.
-# write a function that produces your final score as a percentage given:
-# total: the number of quizzes
-# correct: the number of quizzes you answered
-# wrong: the number of quizzes you answered incorrectly 
+# #FUNCTION ASSIGNMENT 2 SCORE OF BEST 75% DAYS
+# # The same question as previously, however, we only take your best 75% days to account for sick days. No one is ever healthy every day. That means that even if they didn't get every question correct, as long as they got 75% of the questions correct, they will get 100%.
+# # write a function that produces your final score as a percentage given:
+# # total: the number of quizzes
+# # correct: the number of quizzes you answered
+# # wrong: the number of quizzes you answered incorrectly 
 
-def score75(total, correct, wrong):
-    return ((min((total * 0.75), correct) * 2) + min(wrong, ((total * 0.75) - min((total * 0.75), correct)))) * 100 / ((total * 2) * 0.75)
-
-
-
-# print(score75(20,20,0))
-# print(score75(20,15,0))
-# print(score75(20,15,5))
-# print(score75(20,10,5))
-# print(score75(20,0,20))
-
-#CONTROL FLOW IF ELSE STATEMENTS AND WHILE LOOPS
-#else if() in python is 'elif' fucking gross
-def can_watch_movies(age, with_parents):
-    if age >= 17 or with_parents:
-        return True
-    else:
-        return False
-
-#while loop
-# def eat_apples(num__of_apples, on_diet):
-#     apples_remaining = num__of_apples
-#     while apples_remaining > 0 and not on_diet:
-#         apples_remaining -= 1
-#         print('Thank you!')
-#     print('Done')
-#     return
-
-# print(eat_apples(5, False))
-
-#CONTROL FLOW PROBLEM 1: FIZZBUZZ
-#write a pythong function with iterates the integers from 0 to n. For multiples of 3 print 'fizz' isntead of the number, and for the multiples of five, print 'buzz'. For numbers which are multiple of both three and five, print 'fizzbuzz'
-# def fizzbuzz(n):
-#     i = 1
-#     while i <= n:
-#         if i%3 == 0 & i%5 == 0:
-#             # print('fizzbuzz')
-#         elif i%3 == 0:
-#             # print ('fizz')
-#         elif i%5 == 0:
-#             # print('buzz')
-#         else: 
-#             # print(i)
-#         i+= 1
-# fizzbuzz(15)
+# def score75(total, correct, wrong):
+#     return ((min((total * 0.75), correct) * 2) + min(wrong, ((total * 0.75) - min((total * 0.75), correct)))) * 100 / ((total * 2) * 0.75)
 
 
 
-#CONTROL FLOW PROBLEM 2: Multiply Without *
-#Write a python function which multiplies two values (a and b) without using the multiplication symbol. Use addition and a while loop to write your function
+# # print(score75(20,20,0))
+# # print(score75(20,15,0))
+# # print(score75(20,15,5))
+# # print(score75(20,10,5))
+# # print(score75(20,0,20))
 
-#CONTROL FLOW PROBLEM 3: Output the first n prime numbers
-#Write a python function that outputs the first n prime numbers
-#prime numbers are > 1 and can only be divided by 1 and itself
-# def prime_numbers(n):
-#     i = 0
-#     x= []
-#     while len(x) < n:
-#         if i > 1 and not n % i == 0:
+# #CONTROL FLOW IF ELSE STATEMENTS AND WHILE LOOPS
+# #else if() in python is 'elif' fucking gross
+# def can_watch_movies(age, with_parents):
+#     if age >= 17 or with_parents:
+#         return True
+#     else:
+#         return False
+
+# #while loop
+# # def eat_apples(num__of_apples, on_diet):
+# #     apples_remaining = num__of_apples
+# #     while apples_remaining > 0 and not on_diet:
+# #         apples_remaining -= 1
+# #         print('Thank you!')
+# #     print('Done')
+# #     return
+
+# # print(eat_apples(5, False))
+
+# #CONTROL FLOW PROBLEM 1: FIZZBUZZ
+# #write a pythong function with iterates the integers from 0 to n. For multiples of 3 print 'fizz' isntead of the number, and for the multiples of five, print 'buzz'. For numbers which are multiple of both three and five, print 'fizzbuzz'
+# # def fizzbuzz(n):
+# #     i = 1
+# #     while i <= n:
+# #         if i%3 == 0 & i%5 == 0:
+# #             # print('fizzbuzz')
+# #         elif i%3 == 0:
+# #             # print ('fizz')
+# #         elif i%5 == 0:
+# #             # print('buzz')
+# #         else: 
+# #             # print(i)
+# #         i+= 1
+# # fizzbuzz(15)
+
+
+
+# #CONTROL FLOW PROBLEM 2: Multiply Without *
+# #Write a python function which multiplies two values (a and b) without using the multiplication symbol. Use addition and a while loop to write your function
+
+# #CONTROL FLOW PROBLEM 3: Output the first n prime numbers
+# #Write a python function that outputs the first n prime numbers
+# #prime numbers are > 1 and can only be divided by 1 and itself
+# # def prime_numbers(n):
+# #     i = 0
+# #     x= []
+# #     while len(x) < n:
+# #         if i > 1 and not n % i == 0:
+# #             x.append(i)
+# #         i += 1
+# #         print(x);
+
+# # prime_numbers(5)
+# # prime_numbers(7)
+
+
+# #lists what are they(it's a fucking array)
+# nums = [2,5,14,17,20,35]
+# # print(nums[3])
+# nums [1:4] = [5,14,17] # this gives us a range
+
+
+# #LISTS PROBLEM 1: GENERATE LIST OF NUMBERS
+# # Write a python function that generates a list of numbers that starts at start and stops before end and incremnets by step 
+# def generate_list(start, end ,step):
+#     x = []
+#     i = start
+#     if step > 0:
+#         while i < end:
 #             x.append(i)
-#         i += 1
-#         print(x);
+#             i += step
+#         print(x)
+#         print(step)
+#     else:
+#         while i > end:
+#             x.append(i)
+#             i += step
+#         print(x)
+#     return x
 
-# prime_numbers(5)
-# prime_numbers(7)
+# # generate_list(0,5,1)
+# # [0,1,2,3,4] 
+# # generate_list(0,0,1) 
+# # []
+# # generate_list(5,10,2) 
+# # [5,7,9]
+# # generate_list(10,5,-2) 
+# # [10,8,6]
 
+# # LIST PROBLEM 2 : REVERSE THE LIST 
+# # Write a python function which reverse a list 
+# # note: try doing it without list.reverse(), instead try to do it with a while loops
+# def reverse_list(mylist):
+#     #while the length of the array is great than 0, remove the last element and place it into a new array
+#     new_array = [];
+#     length = len(mylist) - 1
 
-#lists what are they(it's a fucking array)
-nums = [2,5,14,17,20,35]
-# print(nums[3])
-nums [1:4] = [5,14,17] # this gives us a range
-
-
-#LISTS PROBLEM 1: GENERATE LIST OF NUMBERS
-# Write a python function that generates a list of numbers that starts at start and stops before end and incremnets by step 
-def generate_list(start, end ,step):
-    x = []
-    i = start
-    if step > 0:
-        while i < end:
-            x.append(i)
-            i += step
-        print(x)
-        print(step)
-    else:
-        while i > end:
-            x.append(i)
-            i += step
-        print(x)
-    return x
-
-# generate_list(0,5,1)
-# [0,1,2,3,4] 
-# generate_list(0,0,1) 
-# []
-# generate_list(5,10,2) 
-# [5,7,9]
-# generate_list(10,5,-2) 
-# [10,8,6]
-
-# LIST PROBLEM 2 : REVERSE THE LIST 
-# Write a python function which reverse a list 
-# note: try doing it without list.reverse(), instead try to do it with a while loops
-def reverse_list(mylist):
-    #while the length of the array is great than 0, remove the last element and place it into a new array
-    new_array = [];
-    length = len(mylist) - 1
-
-    while length >= 0:
-        new_array.append(mylist[length])
-        length -= 1
+#     while length >= 0:
+#         new_array.append(mylist[length])
+#         length -= 1
     
-    print(new_array)
-    return new_array
+#     print(new_array)
+#     return new_array
 
-# reverse_list([1,2,3,4,5])
-# [5,4,3,2,1]
-# reverse_list([])
-# []
+# # reverse_list([1,2,3,4,5])
+# # [5,4,3,2,1]
+# # reverse_list([])
+# # []
 
-#LOOPS PROBLEM 1 : Is number in list?
-# Write a function that checks if a numnber 'n' is in a list 'lst'
-def is_in_list(n, list):
-    # loop through the list
-    # if list contains 'n' answer  = True else answer = False. I should probably break if n is found
-    answer = False
-    for i in list:
-        if i == n:
-            answer = True
-            break
+# #LOOPS PROBLEM 1 : Is number in list?
+# # Write a function that checks if a numnber 'n' is in a list 'lst'
+# def is_in_list(n, list):
+#     # loop through the list
+#     # if list contains 'n' answer  = True else answer = False. I should probably break if n is found
+#     answer = False
+#     for i in list:
+#         if i == n:
+#             answer = True
+#             break
 
-    print (answer)
-    return answer
+#     print (answer)
+#     return answer
         
 
-# is_in_list(5,[1,2,3,4])
-#False
-# is_in_list(5,[1,5,6])
-#True
+# # is_in_list(5,[1,2,3,4])
+# #False
+# # is_in_list(5,[1,5,6])
+# #True
 
-#LOOPS PROBLEM 2 : INTERSECTION OF LISTS
-#Write a function intersection that consumes two lists of numbers and porduces the list of numbers that are in both lists
-#AKA ARE THERE ANY NUMBERS IN BOTH LISTS
-def intersection(lst1, lst2):
-    answer = []
-    for i in lst1:
-        for j in lst2:
-            if j not in answer and i == j:
-                answer.append(i)
-                break
-    return answer
-# intersection([1,2,3], [2,4,6])
-# [2]
-# intersection([1,2,3], [4,5,6])
+# #LOOPS PROBLEM 2 : INTERSECTION OF LISTS
+# #Write a function intersection that consumes two lists of numbers and porduces the list of numbers that are in both lists
+# #AKA ARE THERE ANY NUMBERS IN BOTH LISTS
+# def intersection(lst1, lst2):
+#     answer = []
+#     for i in lst1:
+#         for j in lst2:
+#             if j not in answer and i == j:
+#                 answer.append(i)
+#                 break
+#     return answer
+# # intersection([1,2,3], [2,4,6])
+# # [2]
+# # intersection([1,2,3], [4,5,6])
+# # []
+# # intersection([2,3,2,4],[2,2,4,1,5,3])
+# # [2,4]
+
+# #matrix
+# vector = [2,3,4,5] #its a bunch of numbers bunched together. this is a 4 dimenisonal vector
+
+# matrix = [[1,2,3],
+#           [4,5,6],
+#           [7,8,9]] # a matrix is a retangular array. this is a 3x3 matrix
+
+# #MATRIX PROBLEM 1 : IS IT A VALID MATRIX?
+# #Write a function that determines if the given list of lists is valid, meaning that for each row, it has the same number of columns
+# def is_valid_matrix(matrix):
+#     answer = ''
+
+#     if len(matrix[0]) == len(matrix[1]) and len(matrix[0]) == len(matrix[2]) and len(matrix[1]) == len(matrix[2]):
+#         answer = True
+#     else: 
+#         answer = False
+
+#     print (answer)
+#     return answer
+
+# # is_valid_matrix([[1,2,3],[4,5,6],[7,8,9]])
+# #True
+# # is_valid_matrix([[1,2,3],[4,5],[7,8,9]])
+# #False
+
+
+# #MATRIX PROBLEM  : MATRIX MULTIPLICATION
+# #Write a function that multiplies two matrices together. 
+# # For matrix multiplication, the number of columns in the first matrix must be equal to the number of rows in the second matrix. 
+# # The result matrix, known as the matrix product, has the number of rows of the first and the number of columns of the second matrix. 
+
+# #There is a formal definition of matrix multiplication on wikipedia. https://en.wikipedia.org/wiki/Matrix_multiplication
+# #Output "None" if the matrices cannot be multiplied together. 
+# # Use the previous function is_valid_matrix to validate the two matrices before you multiply them 
+
+# def create_matrix(num_of_rows, num_of_cols):
+#     #this will create a 0 matrix with certain num of rows and cols
+#     my_matrix = []
+#     for i in range(num_of_rows):
+#         row = []
+#         for j in range(num_of_cols):
+#             row.append(0)
+#         my_matrix.append(row)
+#     return my_matrix
+ 
+ 
+# def matrix_multiply(m, n):
+#     if not is_valid_matrix(m) or not is_valid_matrix(n):
+#         return None
+ 
+#     num_of_rows1 = len(m)
+#     num_of_cols1 = len(m[0])
+ 
+#     num_of_rows2 = len(n)
+#     num_of_cols2 = len(n[0])
+ 
+#     if num_of_cols1 != num_of_rows2:
+#         return None
+
+#     answer = create_matrix(num_of_rows1, num_of_cols2)
+
+#     for i in range( num_of_rows1 ):
+#         for j in range( num_of_cols2 ):
+#             for k in range( num_of_rows2 ):
+#                 answer[i][j] += m[i][k] * n[k][j]
+ 
+#     return answer
+ 
+ 
+# matrix1 = [[1,2,3],[4,5,6]]
+# matrix2 = [[1,2],[3,4],[5,6]]
+
+# # print(matrix_multiply(matrix1, matrix2))
+
+
+#REGRESSION PROBLEM 1
+#Given a list, determine if the list is in increasing order
+
+def is_increasing(lst):
+    # STEP 1: Find the Base case: [] and len(lst) == 1
+    if len(lst) < 2: #basically, if lst == [] or len(lst) == 1:
+        return True
+    # STEP 2: Find simplier problem: 
+    # is the first element bigger than the second one?
+    elif lst[0] < lst[1]:
+        return is_increasing(lst[1:])
+    else:
+        return False
+# print(is_increasing([1,2,3,4]))
+#True
+# print(is_increasing([1,3,2,4]))
+#False
+
+
+#Regression Problem 2: NUmbers Greater Than n
+# Given a list, output a new list that only retains the numbers from the list that is greater than n
+def filter_gt_n(lst, n):
+
+    return
+
+print(filter_gt_n([1,2,3,4],2))
+# [3,4] (2 is not included because it is not greater than 2)
+print(filter_gt_n([2,2,3,3],1))
+# [2,2,3,3]
+print(filter_gt_n([2,2,3,3], 4))
 # []
-# intersection([2,3,2,4],[2,2,4,1,5,3])
-# [2,4]
 
-#matrix
-vector = [2,3,4,5] #its a bunch of numbers bunched together. this is a 4 dimenisonal vector
-
-matrix = [[1,2,3],
-          [4,5,6],
-          [7,8,9]] # a matrix is a retangular array. this is a 3x3 matrix
-
-#MATRIX PROBLEM 1 : IS IT A VALID MATRIX?
-#Write a function that determines if the given list of lists is valid, meaning that for each row, it has the same number of columns
-def is_valid_matrix(matrix):
-    answer = ''
-    
-    if len(matrix[0]) == len(matrix[1]) and len(matrix[0]) == len(matrix[2]) and len(matrix[1]) == len(matrix[2]):
-        answer = True
-    else: 
-        answer = False
-
-    print (answer)
-    return answer
-
-is_valid_matrix([[1,2,3],[4,5,6],[7,8,9]])
-#True
-is_valid_matrix([[1,2,3],[4,5],[7,8,9]])
-#False
-
-
-#MATRIX PROBLEM  : MATRIX MULTIPLICATION
-#Write a function that multiplies two matrices together. For matrix multiplication, the number of columns in the first matrix must be equal to the number of rows in the second matrix. The result matrix, known as the matrix product, has the number of rows of the first and the number of columns of the second matrix. 
-
-#There is a formal definition of matrix multiplication on wikipedia. https://en.wikipedia.org/wiki/Matrix_multiplication
-#Output "None" if the matrices cannot be multiplied together. Use the previous function is_valid_matrix to validate the two matrices 
-# before you multiply them 
-
-# def matrix_multiply(m,n):
-#     # m is the first matrix and n is the second matrix
-
-#     return 
+#RECURSION PROBLEM 3 : FIND THE INTERSECTION OF TWO LISTS
+#this is the same question as to the assignment for loops problem 2, however, this time you cannot use loops or variables.
